a7f02428d3c9a522caa40c4aa33a7ca26ddf1a04
lipika.manglaa <lipika.mangla.web@gmail.com>
2023-11-01 00:43:30 +1100

    changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,24 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URL) ;
-    console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`
-    );
-  } catch (error) {
-    console.log(`Errro in Mongodb ${error}`);
-  }
-};
 
-export default connectDB;
+
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URL) ;
+//     console.log(
+//       `Conneted To Mongodb Databse ${conn.connection.host}`
+//     );
+//   } catch (error) {
+//     console.log(`Errro in Mongodb ${error}`);
+//   }
+// };
+
+// export default connectDB;
+
+
+// const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/ecommercenew');
+
+module.exports = mongoose.connection;

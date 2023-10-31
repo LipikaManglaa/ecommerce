f1e875b167bb944c54633b7817d51f52fc5621cb
lipika.manglaa <lipika.mangla.web@gmail.com>
2023-11-01 03:58:40 +1100

    changes'

--- server/config/db.js
+++ server/config/db.js
@@ -19,7 +19,3 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/ecommercenew');
 
  module.exports = mongoose.connection;
-// var db = mongoose.connection;
-
-
-//  export default db;

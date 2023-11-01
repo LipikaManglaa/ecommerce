ece396b4f22b33c61b9c1ad96bc785f46a44c248
lipika.manglaa <lipika.mangla.web@gmail.com>
2023-11-01 11:45:28 +1100

    changes

--- server/index.js
+++ server/index.js
@@ -36,8 +36,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(routes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
+
+app.use(routes);
 
 //PORT
 app.use(express.static('uploads'));
